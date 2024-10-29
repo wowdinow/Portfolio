8aad42e1c66eb814b26eb2c2fe63765ee60bc3a7
wowdinow <f.wadino@gmail.com>
2024-10-29 22:08:03 +0700

    Addition of Navbar and few Views

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<script>
+export default{
+  
+}
+</script>
+
+<template>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light shadow sticky-top">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Frando Wadino</a>
+        <div class="collapse navbar-collapse d-flex justify-content-center">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="/portfolio">Portfolio</a>
+                </li>
+            </ul>
+        </div>
+        <a class="navbar-brand" href="">CV</a>
+    </div>
+</nav>
+
+</template>
+
+<style >
+.navbar-nav {
+        position: absolute;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+</style>
---
+++ src/views/PortfolioView.vue
@@ -0,0 +1,11 @@
+<script>
+</script>
+
+<template>
+    <div class="container d-flex justify-content-center" style="background-color: aqua;">
+        <h1>Testing2</h1>
+    </div>
+</template>
+
+<style>
+</style>
